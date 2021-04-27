2d69d37f4e48ef414ff990e9866f8929628b5baa
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-04-26 21:36:45 -0400

    added swNavi.js to make folder cleaner

--- public/service-worker.js
+++ public/service-worker.js
@@ -54,7 +54,7 @@ const FILES_TO_CACHE = [
     }
   
     // handle runtime GET requests for data from /api routes
-    if (event.request.url.includes("/api/transactions")) {
+    if (event.request.url.includes("/api/")) {
       // make network request and fallback to cache if network request fails (offline)
       event.respondWith(
         caches.open(RUNTIME_CACHE).then(cache => {
